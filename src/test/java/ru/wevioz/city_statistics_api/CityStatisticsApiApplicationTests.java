ec8f652841f2b7f46279a883828952480ab84ad8
wevioz <wevioz@mail.ru>
2023-06-20 11:13:48 +0300

    Intital commit

---
+++ src/test/java/ru/wevioz/city_statistics_api/CityStatisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wevioz.city_statistics_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityStatisticsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
